a8ebeabcbff731ae6dd62211411f797044f34907
David Thole <dthole@gmail.com>
2016-02-07 16:08:12 -0600

    Some updates to the fsharp project, adding two new projects (one for SQL one for a microorm

--- 2016ORMDemo/FSharpORM/Program.fs
+++ 2016ORMDemo/FSharpORM/Program.fs
@@ -1,7 +1,38 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿open FSharp.Data
+open System
 
-[<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+
+[<Literal>]
+let connectionString = 
+    @"Data Source=localhost;Initial Catalog=AdventureWorks;Integrated Security=True"
+
+do
+    use cmd = new SqlCommandProvider<"
+        SELECT TOP(@topN) FirstName, LastName, SalesYTD 
+        FROM Sales.vSalesPerson
+        WHERE CountryRegionName = @regionName AND SalesYTD > @salesMoreThan 
+        ORDER BY SalesYTD
+        " , connectionString>()
+
+    cmd.Execute(topN = 3L, regionName = "United States", salesMoreThan = 1000000M) |> printfn "%A"
+
+
+(* 
+Section 2.
+*)
+printfn("\n\n")
+
+type ShipMethod = SqlEnumProvider<"
+    SELECT Name, ShipMethodID FROM Purchasing.ShipMethod ORDER BY ShipMethodID", connectionString>
+
+//Combine with SqlCommandProvider
+do 
+    use cmd = new SqlCommandProvider<"
+        SELECT COUNT(*) 
+        FROM Purchasing.PurchaseOrderHeader 
+        WHERE ShipDate > @shippedLaterThan AND ShipMethodID = @shipMethodId
+    ", connectionString>() 
+    //overnight orders shipped since Jan 1, 2008 
+    cmd.Execute( System.DateTime( 2008, 1, 1), ShipMethod.``OVERNIGHT J-FAST``) |> printfn "%A"
+
+Console.ReadLine() |> ignore
